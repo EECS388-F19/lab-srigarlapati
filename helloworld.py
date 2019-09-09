5727f017f73e86de30ce8ede38235d4cc8f8e563
srigarlapati <gsri@umich.edu>
2019-09-09 15:38:26 -0400

    hello world

---
+++ helloworld.py
@@ -0,0 +1,14 @@
+import random
+
+def main():
+    print("Srikanth")
+    x = random.randint(1, 101)
+    y = random.randint(1,101)
+    print(x)
+    print(y)
+    print("Sum = ", x+y)
+    print("Average = ", x+y/2)
+
+
+if __name__ == '__main__':
+    main()
